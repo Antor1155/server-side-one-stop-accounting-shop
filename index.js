753503b5bba7f5dc8f204cc63c254e639966fd86
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-21 23:32:40 +0600

    upto new .env file implementation working perfect

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 const express = require("express")
 const cors = require("cors")
+require("dotenv").config()
+
 const { Resend } = require('resend');
-const resend = new Resend(functions.config().resend.key);
+const resend = new Resend(process.env.RESEND_KEY);
 
 // for firebase-function upload only 
 const functions = require('firebase-functions');
-
-const { GoogleSpreadsheet } = require("google-spreadsheet")
-const creds = require("./gsheet_api_key.json")
-const { JWT } = require("google-auth-library")
-const serviceAuth = new JWT({
-    email: creds.client_email,
-    key: creds.private_key,
-    scopes: [
-        "https://www.googleapis.com/auth/spreadsheets"
-    ]
-
-})
 
 const app = express()
 app.use(express.json())
@@ -24,22 +14,8 @@ app.use(cors())
 
 // test get for spread sheet update
 app.get("/test/testing-for-google-spreadsheets", async (req, res) => {
-    const data = { email: "hello", name: "antor" }
-    // updating google sheet 
-    const doc = new GoogleSpreadsheet("1h_VVzALC56PPncpRaGy_CGWZeXrUm2ACPZqKB6-a3co", serviceAuth)
-    await doc.loadInfo()
-
-    const sheet = doc.sheetsByIndex[0]
-
-    const HEADERS = ['email', 'name', 'phone', 'message', 'business']
-    await sheet.setHeaderRow(HEADERS)
-
-    let dataArray = [
-        { "email": data?.email, "name": data?.name, "phone": data?.phone, "message": data?.message, "business": data?.business }
-    ]
-
-    await sheet.addRows(dataArray)
-    res.json("got the info")
+    
+    res.json(process.env.RESEND_KEY)
 })
 
 // edit product 
@@ -48,22 +24,6 @@ app.post("/formdata/:section", async (req, res) => {
     try {
         const section = req.params.section
         const data = req.body;
-
-        // updating google sheet 
-        const doc = new GoogleSpreadsheet("1h_VVzALC56PPncpRaGy_CGWZeXrUm2ACPZqKB6-a3co", serviceAuth)
-        await doc.loadInfo()
-
-        const sheet = doc.sheetsByIndex[0]
-
-        const HEADERS = ['email', 'name', 'phone', 'message', 'business']
-        await sheet.setHeaderRow(HEADERS)
-
-        let dataArray = [
-            { "email": data?.email, "name": data?.name, "phone": data?.phone, "message": data?.message, "business": data?.business }
-        ]
-
-        await sheet.addRows(dataArray)
-
 
         if (section === "indexbig") {
             resend.emails.send({
@@ -111,6 +71,8 @@ app.post("/formdata/:section", async (req, res) => {
             }).catch(error => console.log("indexbig***: ", error))
 
         }
+
+        res.json("received the data")
 
     } catch (error) {
         console.log("error*** : ", error)
